0749f5a33b15b1fcc320350cd3cdcb533c8e70a3
bustosalex <alexbustos1395@gmail.com>
2016-09-21 10:45:35 -0500

    Started the Project

---
+++ KUSD/KUSD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KUSD
+//
+//  Created by Alex on 9/21/16.
+//  Copyright © 2016 Kenosha Unified School District. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
