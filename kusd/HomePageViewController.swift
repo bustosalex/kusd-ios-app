119fed2f23cc79603e04bddd7bdab28093effb7c
bustosalex <alexbustos1395@gmail.com>
2016-09-28 11:41:10 -0500

    Created the homepage

---
+++ kusd/HomePageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomePageViewController.swift
+//  kusd
+//
+//  Created by Alex on 9/27/16.
+//  Copyright © 2016 uwp. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+    @IBOutlet weak var containerView: UIView!
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    
+}
