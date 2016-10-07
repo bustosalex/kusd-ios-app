f49fa9e4d2d831745ab833026808ab3c0c31e577
mcphe006 <Matt@Matthews-MacBook-Pro.local>
2016-10-07 17:45:00 -0500

    Started making the ui for the school scene

---
+++ kusd/SchoolTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  kusd
+//
+//  Created by Matthew McPherson on 10/7/16.
+//  Copyright © 2016 uwp. All rights reserved.
+//
+
+import UIKit
+
+class SchoolTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
